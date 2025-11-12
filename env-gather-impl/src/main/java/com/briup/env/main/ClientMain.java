1b13962274bc898ffed28caff1ff060cbbea5ebf
xiaoxi <2867384243@qq.com>
2025-11-12 22:41:28 +0800

    修改输出集合

--- env-gather-impl/src/main/java/com/briup/env/main/ClientMain.java
+++ env-gather-impl/src/main/java/com/briup/env/main/ClientMain.java
@@ -21,7 +21,7 @@ public class ClientMain {
         Gather gt = configuration.getGather();
         Collection<Environment> envs = gt.gather();
         //输出集合元素个数
-        log.debug("元素数量: " + envs.size());
+        log.debug("元素的数量: " + envs.size());
 
         //2.发送集合对象到服务器
         //Client client = new ClientImpl();
